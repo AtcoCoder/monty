1b065dd7ade66dcd752f2f399775337db7938468
Omar Jammeh <orms45@gmail.com>
2023-03-27 00:58:46 +0000

    All in one update

--- dlist_func.c
+++ dlist_func.c
@@ -1,123 +1,91 @@
 #include "monty.h"
+#include <stdio.h>
+
 /**
- * add_dnodeint - add node to the beginning of list
-* @head: pointer to first node
-* @n: data inside node
-* Return: pointer to first node
-*/
-stack_t *add_dnodeint(stack_t **head, const int n)
-{
-	stack_t *new;
-
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-		new->prev = NULL;
-		*head = new;
-		return (*head);
-	}
-
-	(*head)->prev = new;
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head = new;
-	return (*head);
-}
-/**
- * delete_dnodeint_at_index - delete node a specific spot
- * @head: pointer to first node on list
- * @index: position to delete
- * Return: 1 if successful, -1 if failure
- */
-int delete_dnodeint_at_index(stack_t **head, unsigned int index)
-{
-	stack_t *tmp;
-	stack_t *tmp2;
-	unsigned int i;
-
-	if (*head == NULL)
-		return (-1);
-
-	tmp = *head;
-
-	if (index == 0)
-	{
-		*head = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = NULL;
-		free(tmp);
-		return (1);
-	}
-	i = 0;
-	while (i < (index - 1))
-	{
-		if (tmp == NULL)
-			return (-1);
-		tmp = tmp->next;
-		i++;
-	}
-	tmp2 = (tmp->next)->next;
-	if (tmp->next->next != NULL)
-		tmp->next->next->prev = tmp;
-	free(tmp->next);
-	tmp->next = tmp2;
-
-	return (1);
-}
-/**
- * add_dnodeint_end - add node to end of list
- * @head: pointer to first node
- * @n: data inside node
- * Return: pointer to first node
+ *add_dnodeint_end - add a note at the end of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
  */
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-	stack_t *tmp = *head;
-	stack_t *new_node;
+	stack_t *temp, *aux;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	if (head == NULL)
 		return (NULL);
-
-	new_node->n = n;
-
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
 	if (*head == NULL)
 	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
 	}
-
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-
-	tmp->next = new_node;
-	new_node->prev = tmp;
-	new_node->next = NULL;
-	return (new_node);
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
 }
+
 /**
- * free_dlistint - free a list
- * @head: pointer to first node
+ *add_dnodeint - add a note at the begining of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
+ */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * free_dlistint - frees the doubly linked list
  *
+ * @head: head of the list
+ * Return: no return
  */
 void free_dlistint(stack_t *head)
 {
 	stack_t *tmp;
 
-	while (head != NULL)
+	while ((tmp = head) != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		head = head->next;
+		free(tmp);
 	}
 }
